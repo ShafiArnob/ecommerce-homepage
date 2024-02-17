d921a37e611372a49502d88fa786943f18e2cf5c
ShafiArnob <shafiarnob@gmail.com>
2024-02-17 16:59:00 +0600

    hero banner

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -1,6 +1,22 @@
 import styles from "../styles/HeroSection.module.css";
+import picture from "../assets/picture.png";
 const HeroBanner = () => {
-  return <div className={styles.heroBanner}>HeroBanner</div>;
+  return (
+    <div className={styles.heroBanner}>
+      <div className={styles.heroText}>
+        <h3>
+          Up to <span className="text-color-primary">70%</span> on Black Friday
+        </h3>
+        <h1>
+          TRENDY <span className="text-color-primary">FASHION</span> COLLECTION
+        </h1>
+        <div>Buy Now</div>
+      </div>
+      <div className={styles.heroImgContainer}>
+        <img src={picture} alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default HeroBanner;
