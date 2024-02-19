ddc6bd2a3f7cbc8080b8d6b80c000e270a3d49bb
ShafiArnob <shafiarnob@gmail.com>
2024-02-19 16:07:00 +0600

    products header

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,6 +1,18 @@
 import styles from "../styles/Products.module.css";
+import { GoArrowRight } from "react-icons/go";
+import { GoArrowLeft } from "react-icons/go";
 const Products = () => {
-  return <div className={styles.products}>Products</div>;
+  return (
+    <div className={styles.products}>
+      <div className={styles.productsHeaderContainer}>
+        <h3>Featured Item</h3>
+        <div className={styles.productsHeaderIconContainer}>
+          <GoArrowLeft />
+          <GoArrowRight />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Products;
