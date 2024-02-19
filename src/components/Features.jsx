0f57a753c54dad6720e248ffe99617c337cc0c6a
ShafiArnob <shafiarnob@gmail.com>
2024-02-19 15:50:35 +0600

    features container

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,35 +1,33 @@
 import styles from "../styles/Features.module.css";
 import { TbTruckDelivery } from "react-icons/tb";
-
-const FeatureCard = ({ feature }) => {
-  return <div className={styles.featureCard}>hello</div>;
-};
-
+import { IoChatboxEllipsesOutline } from "react-icons/io5";
+import { PiHandCoinsDuotone } from "react-icons/pi";
+import { BsBox2 } from "react-icons/bs";
 const Features = () => {
   const features = [
     {
       id: 1,
       title: "Free Shipping",
       subtitle: "Minimum order $90",
-      icon: <TbTruckDelivery />,
+      icon: TbTruckDelivery,
     },
     {
       id: 2,
-      title: "Free Shipping",
-      subtitle: "Minimum order $90",
-      icon: <TbTruckDelivery />,
+      title: "24/7 Support",
+      subtitle: "Contact us 24 hours",
+      icon: IoChatboxEllipsesOutline,
     },
     {
       id: 3,
-      title: "Free Shipping",
-      subtitle: "Minimum order $90",
-      icon: <TbTruckDelivery />,
+      title: "Best prices and offers",
+      subtitle: "Order $100 or more",
+      icon: PiHandCoinsDuotone,
     },
     {
       id: 4,
-      title: "Free Shipping",
-      subtitle: "Minimum order $90",
-      icon: <TbTruckDelivery />,
+      title: "Easy Returns",
+      subtitle: "Within 30 days",
+      icon: BsBox2,
     },
   ];
   return (
@@ -42,3 +40,17 @@ const Features = () => {
 };
 
 export default Features;
+
+const FeatureCard = ({ feature }) => {
+  return (
+    <div className={styles.featureCard}>
+      <div className={styles.featureCardIconContainer}>
+        <feature.icon />
+      </div>
+      <div className={styles.featureCardTextContainer}>
+        <h3>{feature.title}</h3>
+        <p>{feature.subtitle}</p>
+      </div>
+    </div>
+  );
+};
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import styles from "../styles/Products.module.css";
 const Products = () => {
   return <div className={styles.products}>Products</div>;
