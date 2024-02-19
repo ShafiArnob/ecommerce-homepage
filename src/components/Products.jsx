94f1273635a42b6bbe220e07fcd0c98bb26ac433
ShafiArnob <shafiarnob@gmail.com>
2024-02-19 18:11:16 +0600

    fetch products

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from "react";
 import styles from "../styles/Products.module.css";
 import { GoArrowRight } from "react-icons/go";
 import { GoArrowLeft } from "react-icons/go";
+import { getAllProducts } from "../api/products";
 const Products = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    const fetchUsers = async () => {
+      const products = await getAllProducts();
+      if (products == null) {
+        setIsError(true);
+        setLoading(false);
+      }
+      setProducts(products);
+      setLoading(false);
+    };
+    fetchUsers();
+  }, []);
+  console.log(products);
   return (
     <div className={styles.products}>
       <div className={styles.productsHeaderContainer}>
